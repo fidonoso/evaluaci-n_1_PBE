2bbcd41dfce20e76f130062813931a565ec20919
Fernando Donoso <fidonoso@gmail.com>
2022-04-22 20:20:36 -0400

    Evaluacion 1 PBE final

--- resources/utils.js
+++ resources/utils.js
@@ -1,35 +1,3 @@
-// const forbidden = [
-//     "parseInt", "document.write", "eval", "isNaN", "unescape", "escape",
-//     "parseFloat", "parseInt", "eval", "isNaN", "onload", "alert", "script",
-//     "<",  ">", "onload", "=", "(", ")", '"' ,"'" ,"/"]
-
-// const getForbidden = () =>{
-//     return forbidden
-// }
-
-// const reemplazarTodos = (palabra,reemplazar) =>{
-
-//     while(palabra.includes(reemplazar)){
-//         console.log(`Se ha reemplazado ${reemplazar} en ${palabra}`)
-//         palabra = palabra.replace(reemplazar,'')
-//     }
-//     return palabra
-// }
-// const limpiarRequest = (data) =>{
-    
-//     forbidden.forEach(word => {
-//         var usuario = data.usuario
-//         var password = data.password 
-//         usuario = reemplazarTodos(usuario,word)
-//         password = reemplazarTodos(password,word)
-//         data = {
-//             usuario,
-//             password
-//         }
-//     })
-//     return data
-    
-// }
 const limpiador = (obj) => {
     console.log(obj)
     const forbidden = ["parseInt", "document.write", "eval", "isNaN", "unescape", "escape", "parseFloat", "parseInt", "eval", "isNaN", "onload", "alert", "script", "<", ">", "onload", "=", "(", ")", '"', "'", "/", ];
